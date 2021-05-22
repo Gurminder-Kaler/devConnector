db92e9c62772d6447bd6aa8dc032f1126c3fef0a
Gurminder Kaler <chaos1.champ@gmail.com>
2021-05-22 09:53:22 +0530

    2nd

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    "mongoURI": "mongodb+srv://admin:chetu@111@cluster0.yegko.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
-app.get('/', () => res.send('Hellow'));
+// connect to mongoDb
+mongoose
+.connect(db)
+.then ( ()=> console.log('MongoDb Connected'))
+.catch (err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello2'));
+
+// use Routes
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.port || 5000;
 
